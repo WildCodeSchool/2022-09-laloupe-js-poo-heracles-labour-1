e28451400d928ef1cbd2db2edc2fc02a7144d5d0
Xavier Georget <xageorget@gmail.com>
2022-10-06 11:07:04 +0200

    Fighter class created and first instanciation of heracles and nemean

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require("./src/Fighter.js");
+
+const heracles = new Fighter("🧔 Heracles", 20, 6);
+const nemean = new Fighter("🦁 Nemean Lion", 11, 13);
+
+heracles.displayInfo();
+nemean.displayInfo();
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,17 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  displayInfo() {
+    console.log(`${this.name} has ${this.life} 💙`);
+  }
+}
+
+module.exports = Fighter;
